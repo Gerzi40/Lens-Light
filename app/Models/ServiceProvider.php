b8a3950fe174af3f743245b2b64ee1fbc89eeed9
Dikoi <dicky.susanto@binus.ac.id>
2024-10-23 21:31:48 +0700

    testing

--- app/Models/ServiceProvider.php
+++ app/Models/ServiceProvider.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServiceProvider extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'short_description', 'category', 'requirement',
+        'rating', 'email', 'whatsapp_number'
+    ];
+
+    public function packages()
+    {
+        return $this->hasMany(Package::class);
+    }
 }
